e6b4b9bfc43b8c9217b1483228a70f6d30ec1556
BartekPawlak <bartosz.pawlak@vp.pl>
2019-01-06 16:46:42 +0100

    Using HTML in R.

--- RProject/app.R
+++ RProject/app.R
@@ -2,4 +2,5 @@ library(shiny)
 library(jsonlite)
 
 #Jest to wersja projektu bez u¿ycia mongoDB
-runApp("NPR/NPR")
+#runApp("NPR/NPR")
+shinyApp(ui = htmlTemplate("www/index.html"), server)
--- RProject/ui.R
+++ RProject/ui.R
@@ -1,32 +1,32 @@
-library(shiny)
+#library(shiny)
 
 # Define UI for app that draws a histogram ----
-ui <- fluidPage(
+#ui <- fluidPage(
 
 # App title ----
-  titlePanel("Hello Shiny!"),
+#  titlePanel("Hello Shiny!"),
 
   # Sidebar layout with input and output definitions ----
-  sidebarLayout(
+#  sidebarLayout(
 
   # Sidebar panel for inputs ----
-	sidebarPanel(
+#	sidebarPanel(
 
 	# Input: Slider for the number of bins ----
-	  sliderInput(inputId = "bins",
-				  label = "Number of bins:",
-				  min = 1,
-				  max = 50,
-				  value = 30)
+#	  sliderInput(inputId = "bins",
+#				  label = "Number of bins:",
+#				  min = 1,
+#				  max = 50,
+#				  value = 30)
 
-			  	),
+#			  	),
 
 				# Main panel for displaying outputs ----
-	mainPanel(
+#	mainPanel(
 
 	# Output: Histogram ----
-	  plotOutput(outputId = "distPlot")
+#	  plotOutput(outputId = "distPlot")
 
-  	)
-  )
-)
+ # 	)
+#  )
+#)
