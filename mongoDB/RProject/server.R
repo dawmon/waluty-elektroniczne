16792a127d9068a74674d945c855e01c372d4e94
Kajetan Krzewina <kolacja11@gmail.com>
2019-01-08 11:25:40 +0100

    deleted od unused code

--- mongoDB/RProject/server.R
+++ mongoDB/RProject/server.R
@@ -12,16 +12,6 @@ server <- function(input, output) {
 
     library(jsonlite)
 
-	output$distPlot <- renderPlot({
-
-		x <- faithful$waiting
-		bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-		hist(x, breaks = bins, col = "#75AADB", border = "white",
-		 xlab = "Waiting time to next eruption (in mins)",
-		 main = "Histogram of waiting times")
-
-    })
 
     output$test <- renderPrint({
 
