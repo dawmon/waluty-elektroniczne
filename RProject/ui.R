21491ca53b2cc132c904e8f0a70efdbfad5bc2b2
BartekPawlak <43841059+BartekPawlak@users.noreply.github.com>
2019-01-11 22:54:07 +0100

    Add files via upload

--- RProject/ui.R
+++ RProject/ui.R
@@ -1,16 +1,19 @@
-#library(shiny)
+library(shiny)
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
 
 # App title ----
-  titlePanel("Hello Shiny!"),
-
+  #titlePanel("Kurs Bitcoin"),
+#div(HTML("Old <em>Faithful Geyser</em> Data")),
+titlePanel(p(strong(em("Kurs Bitconia ")))),
+#p(strong("bold font "), "italic font")),
+   # p(code("code block")),
   # Sidebar layout with input and output definitions ----
 #  sidebarLayout(
 
   # Sidebar panel for inputs ----
-#	sidebarPanel(
+	#sidebarPanel(
 
 	# Input: Slider for the number of bins ----
 #	  sliderInput(inputId = "bins",
@@ -18,15 +21,13 @@ ui <- fluidPage(
 #				  min = 1,
 #				  max = 50,
 #				  value = 30)
-
-#			  	),
+#tags$h1("My header")
+		#	  	),
 
 				# Main panel for displaying outputs ----
-	mainPanel(
 
-	# Output: Histogram ----
-	  plotOutput(outputId = "distPlot")
-
+    mainPanel(
+      plotOutput(outputId = "distPlot")
   	)
   )
 #)
