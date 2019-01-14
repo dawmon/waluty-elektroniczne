f6c8f115c63442243c73b026fb2fabccc953795e
patbrz <patrycja3997@o2.pl>
2019-01-14 19:41:45 +0100

    poprawa wyświetlania

--- Shiny/RProject/RProject/ui.R
+++ Shiny/RProject/RProject/ui.R
@@ -1,16 +1,18 @@
 library(shiny)
+library(jsonlite)
 
 ui <- fluidPage(
   titlePanel(title = h1(strong("Waluty elektroniczne"))),
 
   sidebarLayout(
+
     sidebarPanel(width = 3,
         selectInput("select", h3("Waluta"), choices = list("Bitcoin", "Inne"), selected = "Bitcoin"),
-        dateInput("date", h3("Data"), value = Sys.Date())
+        dateInput("date", h3("Data"), value = Sys.Date() - 1)
     ),
+
     mainPanel(
               h2(htmlOutput("selected_select")),
-
               plotOutput("display")
   	)
   )
